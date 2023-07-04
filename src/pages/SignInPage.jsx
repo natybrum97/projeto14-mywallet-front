a2df8295d4e1646146893bebff08f74e5d43bd6c
Nathália Brum <natybrum.97@gmail.com>
2023-07-04 03:42:08 -0300

    Versão com Cadastro e Login

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,20 +1,62 @@
+import { useState } from "react";
 import styled from "styled-components"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import MyWalletLogo from "../components/MyWalletLogo"
+import axios from "axios";
 
 export default function SignInPage() {
+
+  const navigate = useNavigate();
+
+  const [login, setLogin] = useState({});
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  function enviarInfos(e) {
+    e.preventDefault();
+
+    const obj = {
+        email: email,
+        password: senha
+    }
+
+
+    const url = "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login";
+
+    const promise = axios.post(url, obj);
+
+    promise.then(resposta => {
+       
+        setLogin(resposta.data);
+        console.log(resposta.data, "lista");
+        navigate("/home");
+
+    });
+
+    promise.catch(erro => {
+        
+        alert('Usuário e/ou senha inválidos!');
+        console.log(erro.response.data);
+    });
+
+}
+
   return (
     <SingInContainer>
-      <form>
+
+      <form onSubmit={enviarInfos}>
+
         <MyWalletLogo />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
+        <input placeholder="E-mail" type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        <input placeholder="Senha" type="password" autoComplete="new-password" id="senha" value={senha} onChange={(e) => setSenha(e.target.value)} required />
         <button>Entrar</button>
+
       </form>
 
-      <Link>
+      <Link to='/cadastro'>
         Primeira vez? Cadastre-se!
       </Link>
+
     </SingInContainer>
   )
 }
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,20 +1,62 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import styled from "styled-components"
 import MyWalletLogo from "../components/MyWalletLogo"
+import { useState } from "react";
+import axios from "axios";
 
 export default function SignUpPage() {
+
+  const navigate = useNavigate();
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+  const [confirmar, setConfirmar] = useState('');
+
+  function enviarInfos(e) {
+    e.preventDefault();
+
+    const obj = {
+      name: name,
+      email: email,
+      password: senha,
+      confirmar: confirmar
+    }
+
+    const apiUrl = "https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/sign-up";
+
+    const promise = axios.post(apiUrl, obj);
+
+    promise.then(resposta => {
+
+      alert('Você foi cadastrado com sucesso!')
+      console.log(resposta.data);
+      navigate("/");
+
+    });
+
+    promise.catch(erro => {
+    
+      console.log(erro.response.data);
+      alert(erro.response.data.message);
+
+    });
+  }
+
   return (
     <SingUpContainer>
-      <form>
+      <form onSubmit={enviarInfos}>
+
         <MyWalletLogo />
-        <input placeholder="Nome" type="text" />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" />
-        <button>Cadastrar</button>
+        <input placeholder="Nome" type="text" id="nome" value={name} onChange={(e) => setName(e.target.value)} required />
+        <input placeholder="E-mail" type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        <input placeholder="Senha" type="password" autoComplete="new-password" id="senha" value={senha} onChange={(e) => setSenha(e.target.value)} required />
+        <input placeholder="Confirme a senha" type="password" autoComplete="new-password" id="confirmar" value={confirmar} onChange={(e) => setConfirmar(e.target.value)} required />
+        <button type="submit">Cadastrar</button>
+
       </form>
 
-      <Link>
+      <Link to='/'>
         Já tem uma conta? Entre agora!
       </Link>
     </SingUpContainer>
