52f7711f573621cd5c88c2654ff1af0cfb593fe7
Nathália Brum <natybrum.97@gmail.com>
2023-07-09 19:27:11 -0300

    Correcao 1

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -76,7 +76,7 @@ export default function HomePage(props) {
 
           console.log(response.data);
           setTransacao(response.data);
-          navigate(`/editar-registro/${response.data.tipo}/:${transacaoId}`);
+          navigate(`/editar-registro/${response.data.tipo}/${transacaoId}`);
           
         })
         .catch((error) => {
